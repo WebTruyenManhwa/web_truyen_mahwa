f273dc490ab9751df79b80f22a46e00d0d0989c9
MinhNQ03 (OMI.BU5) <nguyenquangminh28092001@gmail.com>
2025-08-01 12:10:23 +0700

    [FixMigate] fix migrate

--- backend/db/migrate/20231201000000_create_chat_messages.rb
+++ backend/db/migrate/20231201000000_create_chat_messages.rb
@@ -1,4 +1,4 @@
-class CreateChatMessages < ActiveRecord::Migration[7.0]
+class CreateChatMessages < ActiveRecord::Migration[8.0]
   def change
     create_table :chat_messages do |t|
       t.text :content
